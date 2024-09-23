30aea82ae691e6eb7f3b487762d68eba71c8d357
Pedro Augusto <pa.severosilva@gmail.com>
2024-09-22 21:20:57 -0300

    Utilizando o metodo async e await

--- js server/main.js
+++ js server/main.js
@@ -1,3 +1,14 @@
-const response = fetch("http://localhost:3333/products")
+//Utilizando o Fetch com o then.
+/*const response = fetch("http://localhost:3333/products")
 .then((response) => response.json())
 .then((data) => console.log(data))
+*/
+
+//Utilizando o fetch com asyn/await.
+async function fetchProducts() {
+    const response = await fetch("http://localhost:3333/products");
+    const data = await response.json();
+    console.log(data);
+}
+
+fetchProducts();
